86d381f749623f52b0757c679722426ec7213461
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-19 09:45:46 +0200

    Question01

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_LINQ02.Models
+{
+    internal class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+        public override string ToString() => $"{OrderID}: {OrderDate:d} for {Total:C2}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace G_NET_12_LINQ02
+﻿using G_NET_12_LINQ02.Models;
+using static G_NET_12_LINQ02.DataSources.Source;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Xml.Linq;
+namespace G_NET_12_LINQ02
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+            #region Question01
+
+            ////1. Get top 3 most expensive products
+            //var result1 = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+            //foreach (var item in result1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+
         }
     }
 }
