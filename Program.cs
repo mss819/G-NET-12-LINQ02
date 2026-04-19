d7fc973530a2d0266b2a4c8915dcceeab3c730f8
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-19 10:35:36 +0200

    Question13

--- Program.cs
+++ Program.cs
@@ -168,6 +168,18 @@ namespace G_NET_12_LINQ02
 
             #endregion
 
+            #region Question13
+
+            //13.find product IDs that are in setA but NOT in setB
+            //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            //int[] setB = { 3, 6, 9, 12, 15, 13 };
+            //var result = setA.Except(setB);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
         }
     }
 }
